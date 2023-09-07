c9ccb09ba05119475d3321c7e60e9dbbdae8a92f
ying <leftleft0813@gmail.com>
2023-09-08 01:16:01 +0800

    week2 fix js & css

--- static/js/index.js
+++ static/js/index.js
@@ -65,13 +65,13 @@ function getMrt() {
 
       carousel.innerHTML = "";
 
-      //每個捷運站名都用 span 包起來
+ 
       data.data[0].forEach((station) => {
         let spanElement = document.createElement("span");
         spanElement.textContent = station;
         carousel.appendChild(spanElement);
 
-        // 點選 span 時，searchInput = station -> 執行搜尋
+
         spanElement.addEventListener("click", () => {
           document.querySelector("#searchInput").value = station;
           searchAttractions(station);
@@ -98,62 +98,81 @@ function searchAttractions(stationName) {
 
 let attraction = document.querySelector(".attraction");
 
-function getData(url) {
+function fetchData(url) {
+  if(!fetching){
+    fetching = true;
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            // 遍歷每一個區塊
-
-            for (let index = 0; index < data.data.length; index++) {
-                // 創建attractionCard
-                let attractionCard = document.createElement("div");
-                attractionCard.setAttribute("class", "attractionCard");
-
-                // 圖片區域 attractionCard > img
-                let atrractionImg = document.createElement("img");
-                attractionCard.appendChild(atrractionImg);
-
-                // 將圖片網址切到只剩第一個圖片
-                let src = data.data[index].images[0].split(',')[0];
-                atrractionImg.setAttribute("src", src);
-
-                // 文字區域
-                // 景點名稱 attractionCard > attractionName 
-                let attractionName = document.createElement("div");
-                attractionCard.appendChild(attractionName);
-                attractionName.setAttribute("class", "attractionName");
-                attractionName.textContent = data.data[index].name;
-
-                // mrt & 類別用同一個div包起來 attractionCard > attractionInfo
-                let attractionInfo = document.createElement("div");
-                attractionCard.appendChild(attractionInfo);
-                attractionInfo.setAttribute("class", "attractionInfo");
-
-                // mrt名稱 attractionInfo > attractionMrt
-                let attractionMrt = document.createElement("div");
-                attractionInfo.appendChild(attractionMrt);
-                attractionMrt.setAttribute("class", "attractionMrt");
-                attractionMrt.textContent = data.data[index].mrt;
-
-                // 類別 attractionInfo > attractionCat
-                let attractionCat = document.createElement("div");
-                attractionInfo.appendChild(attractionCat);
-                attractionCat.setAttribute("class", "attractionCat");
-                attractionCat.textContent = data.data[index].category;
-
-                // 將新的資料附加到現有資料上
-                attraction.appendChild(attractionCard);
+            fetching = false;
+            if(data.data.length ===0){
+              displayErrorMessage("找不到搜尋結果，請重試");
+            }else{
+              getData(data);
+              fetching = false;
             }
-
-            // 更新 nextPage
-            nextPage = data.nextPage; 
-            if (nextPage === null) {
-                observer.unobserve(footer);
-            }
-
-            // 解除 fetching 狀態
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
             fetching = false;
         });
+
+  }
+
+}
+
+
+function getData(data) {
+
+  for (let index = 0; index < data.data.length; index++) {
+      // 創建attractionCard
+      let attractionCard = document.createElement("div");
+      attractionCard.setAttribute("class", "attractionCard");
+
+      // 圖片區域 attractionCard > img
+      let atrractionImg = document.createElement("img");
+      attractionCard.appendChild(atrractionImg);
+
+      // 將圖片網址切到只剩第一個圖片
+      let src = data.data[index].images[0].split(',')[0];
+      atrractionImg.setAttribute("src", src);
+
+      // 文字區域
+      // 景點名稱 attractionCard > attractionName 
+      let attractionName = document.createElement("div");
+      attractionCard.appendChild(attractionName);
+      attractionName.setAttribute("class", "attractionName");
+      attractionName.textContent = data.data[index].name;
+
+      // mrt & 類別用同一個div包起來 attractionCard > attractionInfo
+      let attractionInfo = document.createElement("div");
+      attractionCard.appendChild(attractionInfo);
+      attractionInfo.setAttribute("class", "attractionInfo");
+
+      // mrt名稱 attractionInfo > attractionMrt
+      let attractionMrt = document.createElement("div");
+      attractionInfo.appendChild(attractionMrt);
+      attractionMrt.setAttribute("class", "attractionMrt");
+      attractionMrt.textContent = data.data[index].mrt;
+
+      // 類別 attractionInfo > attractionCat
+      let attractionCat = document.createElement("div");
+      attractionInfo.appendChild(attractionCat);
+      attractionCat.setAttribute("class", "attractionCat");
+      attractionCat.textContent = data.data[index].category;
+
+      // 將新的資料附加到現有資料上
+      attraction.appendChild(attractionCard);
+  }
+
+  // 更新 nextPage
+  nextPage = data.nextPage; 
+  if (nextPage === null) {
+      observer.unobserve(footer);
+  }
+
+  fetching = false;
+;
 }
 
 
@@ -171,38 +190,19 @@ const observer = new IntersectionObserver(infiniteScroll, options);
 observer.observe(footer);
 
 function infiniteScroll(entries) {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            if (fetching === false) {
-                if (!keyword) {
-                    const url = `/api/attractions?page=${nextPage}`; 
-                    fetchData(url);
-                } else {
-                    const url = `/api/attractions?page=${nextPage}&keyword=${encodeURIComponent(keyword)}`;
-                    fetchData(url);
-                }
-                nextPage++;
-            }
-        }
-    });
-}
-
-function fetchData(url) {
-    fetching = true;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            fetching = false;
-            if(data.data.length ===0){
-              displayErrorMessage("找不到搜尋結果，請重試");
-            }else{
-              getData(url);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            fetching = false;
-        });
+  entries.forEach((entry) => {
+      if (entry.isIntersecting && !fetching) {
+          if (!keyword) {
+              const url = `/api/attractions?page=${nextPage}`;
+              fetchData(url);
+          } else {
+              const url = `/api/attractions?page=${nextPage}&keyword=${encodeURIComponent(keyword)}`;
+              fetchData(url);
+          }
+          nextPage++;
+          fetching = true; 
+      }
+  });
 }
 
 

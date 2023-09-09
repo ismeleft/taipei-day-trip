2ef4c79f9bfbbbc078c91d450868a5d6df9565a9
ying <leftleft0813@gmail.com>
2023-09-09 11:47:05 +0800

    fix infinite scroll

--- static/js/index.js
+++ static/js/index.js
@@ -1,53 +1,53 @@
 let fetching = false;
-let nextPage = 0;
-let attractionSize = 12;
-let keyword ="";
+let nextPage =0 ;
+let keyword;
 
+//建立list-bar，左右滾動的功能
 let listBar = document.querySelector(".list-bar");
 let buttonPrev = document.querySelector(".btn-prev");
 let buttonNext = document.querySelector(".btn-next");
-let carousel = document.querySelector(".carousel");
+let carousel = document.querySelector(".carousel"); 
 
-let currentIndex = 0;
-let data;
+let currentIndex =0 ; 
+let data; 
 
 buttonPrev.addEventListener("click", () => {
-  if (currentIndex === 0) {
-    return;
+  if (currentIndex === 0){
+    return
   }
   currentIndex -= 1 / data.data[0].length;
   console.log(currentIndex);
-  slideMrt(currentIndex, data);
+  slideMrt(currentIndex, data); 
 });
 
 buttonNext.addEventListener("click", () => {
-  if (window.innerWidth < 600) {
-    if (currentIndex === 0.3125) {
-      return;
-    }
-  } else if (window.innerWidth < 1200) {
-    if (currentIndex === 0.1875) {
-      return;
-    }
-  } else {
-    if (currentIndex === 0.0625) {
-      return;
-    }
+  if (window.innerWidth < 600 ){
+    if (currentIndex === 0.3125 ){
+      return
+    };
+  }else if(window.innerWidth < 1200){ 
+    if (currentIndex === 0.1875){
+      return
+    };
+  }else{
+    if (currentIndex === 0.0625){
+      return
+    };
   }
 
   currentIndex += 1 / data.data[0].length;
   console.log(currentIndex);
-  slideMrt(currentIndex, data);
+  slideMrt(currentIndex, data); 
 });
 
-const slideMrt = (currentIndex, data) => {
+const slideMrt = (currentIndex, data) => { 
   currentIndex = (currentIndex + data.data[0].length) % data.data[0].length;
-  if (window.innerWidth < 600) {
-    carousel.style.transform = `translate(-${currentIndex * 3 * 100}%)`;
-  } else if (window.innerWidth < 1200) {
-    carousel.style.transform = `translate(-${currentIndex * 5 * 100}%)`;
-  } else {
-    carousel.style.transform = `translate(-${currentIndex * 14 * 100}%)`;
+  if (window.innerWidth < 600 ){
+    carousel.style.transform = `translate(-${(currentIndex)* 3*100}%)`;
+  }else if(window.innerWidth < 1200){ 
+    carousel.style.transform = `translate(-${(currentIndex)* 5*100}%)`;
+  }else{
+    carousel.style.transform = `translate(-${(currentIndex)* 14*100}%)`;
   }
 };
 
@@ -56,16 +56,18 @@ function getMrt() {
   fetch("/api/mrts")
     .then((response) => response.json())
     .then((responseData) => {
-      data = responseData;
+      data = responseData; 
       console.log(data.data[0]);
 
       carousel.innerHTML = "";
 
+      //每個捷運站名都用 span 包起來
       data.data[0].forEach((station) => {
         let spanElement = document.createElement("span");
         spanElement.textContent = station;
         carousel.appendChild(spanElement);
 
+        // 點選 span 時，searchInput = station -> 執行搜尋
         spanElement.addEventListener("click", () => {
           document.querySelector("#searchInput").value = station;
           searchAttractions(station);
@@ -76,71 +78,113 @@ function getMrt() {
 
 getMrt();
 
-let attraction = document.querySelector(".attraction");
+function searchAttractions(stationName) {
+  keyword = stationName; 
+  nextPage = 0; 
+  attraction.innerHTML = ""; 
 
-function fetchAttractions(url) {
+  if (!keyword) {
+    fetchData(`/api/attractions?page=${nextPage}`);
+  } else {
+    fetchData(`/api/attractions?page=${nextPage}&keyword=${encodeURIComponent(keyword)}`);
+  }
+}
+
+//關鍵字搜尋
+const searchBtn = document.querySelector(".searchBtn")
+searchBtn.addEventListener("click",()=>{
+  const searchInput = document.querySelector("#searchInput").value;
+  keyword = searchInput; 
+  nextPage = 0; 
+  attraction.innerHTML = ''; 
+  searchAttractions(searchInput);
+  observer.observe(footer);
+
+})
+
+//從api/atrractions抓資料
+function fetchData(url) {
   fetching = true;
   fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      fetching = false;
-      if (data.data.length === 0) {
-        displayErrorMessage("找不到搜尋結果，請重試");
-      } else {
-        handleAttractionData(data);
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-      fetching = false;
-    });
+      .then(response => response.json())
+      .then(data => {
+          fetching = false;
+          if(data.data.length ===0){
+            displayErrorMessage("找不到搜尋結果，請重試");
+          }else{
+            getData(data);
+          }
+      })
+      .catch(error => {
+          console.error('Error fetching data:', error);
+          fetching = false;
+      });
 }
 
-function handleAttractionData(data) {
+
+//創建attraction區塊
+let attraction = document.querySelector(".attraction");
+function getData(data) {
+
+  // 遍歷每一個區塊
+
   for (let index = 0; index < data.data.length; index++) {
-    let attractionCard = document.createElement("div");
-    attractionCard.setAttribute("class", "attractionCard");
+      // 創建attractionCard
+      let attractionCard = document.createElement("div");
+      attractionCard.setAttribute("class", "attractionCard");
 
-    let atrractionImg = document.createElement("img");
-    attractionCard.appendChild(atrractionImg);
+      // 圖片區域 attractionCard > img
+      let atrractionImg = document.createElement("img");
+      attractionCard.appendChild(atrractionImg);
 
-    let src = data.data[index].images[0].split(",")[0];
-    atrractionImg.setAttribute("src", src);
+      // 將圖片網址切到只剩第一個圖片
+      let src = data.data[index].images[0].split(',')[0];
+      atrractionImg.setAttribute("src", src);
 
-    let attractionName = document.createElement("div");
-    attractionCard.appendChild(attractionName);
-    attractionName.setAttribute("class", "attractionName");
-    attractionName.textContent = data.data[index].name;
+      // 文字區域
+      // 景點名稱 attractionCard > attractionName 
+      let attractionName = document.createElement("div");
+      attractionCard.appendChild(attractionName);
+      attractionName.setAttribute("class", "attractionName");
+      attractionName.textContent = data.data[index].name;
 
-    let attractionInfo = document.createElement("div");
-    attractionCard.appendChild(attractionInfo);
-    attractionInfo.setAttribute("class", "attractionInfo");
+      // mrt & 類別用同一個div包起來 attractionCard > attractionInfo
+      let attractionInfo = document.createElement("div");
+      attractionCard.appendChild(attractionInfo);
+      attractionInfo.setAttribute("class", "attractionInfo");
 
-    let attractionMrt = document.createElement("div");
-    attractionInfo.appendChild(attractionMrt);
-    attractionMrt.setAttribute("class", "attractionMrt");
-    attractionMrt.textContent = data.data[index].mrt;
+      // mrt名稱 attractionInfo > attractionMrt
+      let attractionMrt = document.createElement("div");
+      attractionInfo.appendChild(attractionMrt);
+      attractionMrt.setAttribute("class", "attractionMrt");
+      attractionMrt.textContent = data.data[index].mrt;
 
-    let attractionCat = document.createElement("div");
-    attractionInfo.appendChild(attractionCat);
-    attractionCat.setAttribute("class", "attractionCat");
-    attractionCat.textContent = data.data[index].category;
+      // 類別 attractionInfo > attractionCat
+      let attractionCat = document.createElement("div");
+      attractionInfo.appendChild(attractionCat);
+      attractionCat.setAttribute("class", "attractionCat");
+      attractionCat.textContent = data.data[index].category;
 
-    attraction.appendChild(attractionCard);
+      // 將新的資料附加到現有資料上
+      attraction.appendChild(attractionCard);
   }
 
-  nextPage = data.nextPage;
+  // 更新 nextPage
+  nextPage = data.nextPage; 
   console.log(nextPage);
-
   if (nextPage === null) {
-    observer.unobserve(footer);
+      observer.unobserve(footer);
   }
+
+
 }
 
+
+// infinite scroll
 const options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.5,
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
 };
 
 const footer = document.querySelector("footer");
@@ -149,52 +193,26 @@ const observer = new IntersectionObserver(infiniteScroll, options);
 observer.observe(footer);
 
 function infiniteScroll(entries) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      if (fetching === false) {
-        if (!keyword) {
-          const url = `/api/attractions?page=${nextPage}`;
-          fetchAttractions(url);
-        } else {
-          const url = `/api/attractions?page=${nextPage}&keyword=${encodeURIComponent(keyword)}`;
-          fetchAttractions(url);
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            if (fetching === false) {
+                if (!keyword) {
+                    const url = `/api/attractions?page=${nextPage}`; 
+                    fetchData(url);
+                } else {
+                    const url = `/api/attractions?page=${nextPage}&keyword=${encodeURIComponent(keyword)}`;
+                    fetchData(url);
+                }
+                // nextPage++;
+            }
         }
-        nextPage++;
-      }
-    }
-  });
+    });
 }
 
-// keyword search
-const searchBtn = document.querySelector('.searchBtn');
-searchBtn.addEventListener('click', () => {
-  const searchInput = document.querySelector('#searchInput').value.trim(); // Remove leading/trailing whitespace
-  keyword = searchInput;
-  nextPage = 0;
-  attraction.innerHTML = '';
-  searchAttractions(searchInput);
-});
 
-function searchAttractions(stationName) {
-  if (!stationName) {
-    // If the search input is empty, reset to show all attractions
-    keyword = '';
-    nextPage = 0;
-  } else {
-    keyword = stationName;
-    nextPage = 0;
-  }
-  attraction.innerHTML = '';
-
-  if (!keyword) {
-    fetchAttractions(`/api/attractions?page=${nextPage}`);
-  } else {
-    fetchAttractions(`/api/attractions?page=${nextPage}&keyword=${encodeURIComponent(keyword)}`);
-  }
-}
 // 錯誤訊息的提醒
 function displayErrorMessage(message) {
-  attraction.innerHTML = "";
+  attraction.innerHTML = '';
 
   const errorMessage = document.createElement("div");
   errorMessage.setAttribute("class", "error-message");

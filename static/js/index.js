db14e5d5154a449295c02ab37ef23e86417008dd
ying <leftleft0813@gmail.com>
2023-09-13 23:16:27 +0800

    week3 attraction

--- static/js/index.js
+++ static/js/index.js
@@ -40,8 +40,7 @@ buttonNext.addEventListener("click", () => {
   slideMrt(currentIndex, data); 
 });
 
-const slideMrt = (currentIndex, data) => { 
-  currentIndex = (currentIndex + data.data[0].length) % data.data[0].length;
+const slideMrt = (currentIndex) => { 
   if (window.innerWidth < 600 ){
     carousel.style.transform = `translate(-${(currentIndex)* 3*100}%)`;
   }else if(window.innerWidth < 1200){ 
@@ -101,6 +100,17 @@ searchBtn.addEventListener("click",()=>{
   observer.observe(footer);
 
 })
+//關鍵字搜尋－enter
+searchInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    const searchValue = searchInput.value;
+    keyword = searchValue;
+    nextPage = 0;
+    attraction.innerHTML = "";
+    searchAttractions(searchValue);
+    observer.observe(footer);
+  }
+});
 
 //從api/atrractions抓資料
 function fetchData(url) {
@@ -110,7 +120,7 @@ function fetchData(url) {
       .then(data => {
           fetching = false;
           if(data.data.length ===0){
-            displayErrorMessage("找不到搜尋結果，請重試");
+            displayErrorMessage(`找不到 ${keyword} 的搜尋結果，請重試`);
           }else{
             getData(data);
           }
@@ -125,19 +135,20 @@ function fetchData(url) {
 //創建attraction區塊
 let attraction = document.querySelector(".attraction");
 function getData(data) {
-
-  // 遍歷每一個區塊
-
   for (let index = 0; index < data.data.length; index++) {
       // 創建attractionCard
       let attractionCard = document.createElement("div");
       attractionCard.setAttribute("class", "attractionCard");
+      //連結到/attraction/id
+      attractionCard.addEventListener("click",()=>{
+        let attractionId = data.data[index].id;
+        window.location.href=`/attraction/${attractionId}`
+      })
 
       // 圖片區域 attractionCard > img
       let atrractionImg = document.createElement("img");
       attractionCard.appendChild(atrractionImg);
 
-      // 將圖片網址切到只剩第一個圖片
       let src = data.data[index].images[0].split(',')[0];
       atrractionImg.setAttribute("src", src);
 
@@ -179,7 +190,6 @@ function getData(data) {
 
 }
 
-
 // infinite scroll
 const options = {
     root: null,
@@ -188,7 +198,6 @@ const options = {
 };
 
 const footer = document.querySelector("footer");
-
 const observer = new IntersectionObserver(infiniteScroll, options);
 observer.observe(footer);
 
@@ -203,7 +212,6 @@ function infiniteScroll(entries) {
                     const url = `/api/attractions?page=${nextPage}&keyword=${encodeURIComponent(keyword)}`;
                     fetchData(url);
                 }
-                // nextPage++;
             }
         }
     });
